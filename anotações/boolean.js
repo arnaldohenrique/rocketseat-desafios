52666d6d9bdfc988bad461f580284a8e00f3852c
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-16 11:36:38 -0300

    aprendendo boolean

---
+++ anotações/boolean.js
@@ -0,0 +1,24 @@
+/*
+    Boolean
+    
+        * somente 2 valores
+
+            true    // verdadeiro
+            false   // falso
+*/
+
+//true
+console.log(true)
+
+//false
+console.log(false)
+
+/*  um pequeno teste logico
+
+ - Eu vou fazer um café
+
+ >preciso pegar a chicara
+ >a chicara está suja? true or false
+ >if true lavar
+ >if false colocar café
+*/
