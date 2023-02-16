2ffd19ba6c5803ae06344fdad470fa62d7adc0ea
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-16 12:03:26 -0300

    basico do object

---
+++ anotações/basico do object.js
@@ -0,0 +1,17 @@
+/*
+* Object > basico
+    -Objeto
+    -Propriedades / Atributos > altura, peso, cor, tamanho
+    -Funcionalidades / Métodos > como funciona
+
+    -todo objeto tem atributos e métodos
+    { propriedade: "valor" }
+*/
+
+console.log({
+    name: 'Naldo',
+    idade: 22,
+    andar: function() {
+        console.log('andar')
+    }
+})
