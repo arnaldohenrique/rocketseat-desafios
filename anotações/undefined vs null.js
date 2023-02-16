52c538c6de4097fb1f29ddd97b882031a33d5f90
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-16 11:55:39 -0300

    undefined vs null

---
+++ anotações/undefined vs null.js
@@ -0,0 +1,18 @@
+/*
+* undefined - algo que não existe
+    -indefinido
+
+* null - algo que existe mas não a nada dentro
+    -nulo
+    -objeto que não possui nada dentro
+    -diferente de indefinido
+*/
+
+//undefined
+console.log(undefined)
+
+//null
+console.log(null)
+
+//são diferentes
+console.log(null === undefined)
