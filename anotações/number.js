9d90f6382ebade614d9a368bf17fdc256259650a
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-16 11:31:17 -0300

    aprendendo number

---
+++ anotações/number.js
@@ -0,0 +1,29 @@
+/*
+    Number
+        *números
+
+        33      // inteiros
+        12.5    // reais - float
+        NaN     // Not a Number
+        Infinity// infinito
+*/
+
+//inteiros
+console.log(33)
+console.log(-33)
+console.log(33 + -33)
+
+//reais - float
+console.log(12.5)
+console.log(-12.5)
+console.log(12.5 + -12.5)
+
+//NaN 
+console.log(33 / 'asd')
+
+//Infinity
+console.log(Infinity) 
+//Funciona apenas com 'i' maiusculo, se não vai dar erro de referencia
+
+//inteiros + reais
+console.log(33 + 12.5)
