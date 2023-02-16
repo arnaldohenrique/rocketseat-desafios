2a645568c81d4e4d3ab3204dccab7c9337b0eae7
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-16 11:19:39 -0300

    aprendendo o uso de string

---
+++ anotações/string.js
@@ -0,0 +1,26 @@
+/*
+    String
+
+    *Cadeia de caracteres
+
+    "" // aspas duplas
+    '' // aspas simples
+    `` // template literals ou template strings (famosa crase)
+
+*/
+// caso eu precise usar aspas duplas dentro ex: ('na"l"do')
+console.log('naldo') 
+
+//caso eu precise usar aspas simples dentro ex: ("na'l'do")
+console.log("naldo")
+
+//caso eu precise usar aspas simples e duplas ex: (`na"'l'"do`)
+console.log(`naldo`)
+
+//as crases tambem permitem o uso de multi-linhas e o uso de expressões de linguagem
+console.log(`   n
+                a
+                l
+                d
+                o
+                ${1+1}`)
