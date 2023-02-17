52352061b09410bdd77da186b98c61611c4c6fef
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-17 11:17:23 -0300

    typeof

---
+++ anotações/typeof.js
@@ -0,0 +1,23 @@
+/*
+    * Typeof
+
+    - O JS é uma linguagem francamente tipada e dinâmica
+    - Variáveis não precisam ter um tipo previamente definido
+    - Podemos mudar o conteúdo da variável
+*/
+
+//string
+let clima1 = 'Quente' 
+console.log(typeof clima1)
+
+//number
+let clima2 = 1 
+console.log(typeof clima2)
+
+//boolean
+let clima3 = true
+console.log(typeof clima3)
+
+//você pode ver o tipo da variavel usando 'typeof'
+
+//lembrando que usando const você não consegue mudar o typeof da variavel
