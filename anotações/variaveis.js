024cc053013765a3a015cbca8eb0b47a2716c404
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-17 11:08:27 -0300

    variaveis

---
+++ anotações/variaveis.js
@@ -0,0 +1,27 @@
+/*
+    *Variáveis
+    
+    - Nomes simbólicos para receber algum valor
+    - Atalhos de código
+    - Identificadores
+    - 3 palavras reservadas para criar uma variável
+        >var
+        >let
+        >const
+*/
+
+//var
+var clima1 = 'Quente' 
+console.log(clima1)
+clima1 = 'Frio'
+console.log(clima1)
+
+//let
+let clima2 = 'Frio' 
+console.log(clima2)
+clima2 = 'Quente'
+console.log(clima2)
+
+//const
+const clima3 = 'Chovendo' //const não muda seu valor durante a aplicação
+console.log(clima3)
