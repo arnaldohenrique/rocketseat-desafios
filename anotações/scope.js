2e3fb7f6df7b57e5515179c657deb4b4ae9f8751
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-17 11:30:46 -0300

    scope

---
+++ anotações/scope.js
@@ -0,0 +1,16 @@
+/*
+    *Scope
+
+    - Escopo determina a visibilidade de alguma variável no JS
+*/
+
+//Block statement > declaração de bloco
+
+//vamos iniciar um bloco
+{
+    //aqui dentro é um bloco e posso colocar qualquer código
+} //aqui fechamos o bloco
+
+/*
+    O bloco, tambem criará um novo escopo. Chamamos de 'block-scoped'
+*/
