ba9587ef3a9e3185441f2759d3b2777483362292
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-20 13:33:27 -0300

    como declarar uma variavel

---
+++ anotações/como declarar variavel.js
@@ -0,0 +1,9 @@
+// Variáveis e tipos de dados
+// declaration or declaração
+var name
+
+// assignment or atribuição de valores
+name = "Naldo"
+
+//que tipo de dado foi colocado na variável
+console.log(typeof name)
