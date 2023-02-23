78f65ba1d605181f40a1dca6954030d54261eae1
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-23 04:18:00 -0300

    function scope

---
+++ anotações/function scope.js
@@ -0,0 +1,11 @@
+//function scope
+let subject = 'algo'
+
+function createThink(subject) {
+    return subject = 'study'
+    
+}
+
+console.log(subject)
+console.log(createThink(subject))
+console.log(subject)
