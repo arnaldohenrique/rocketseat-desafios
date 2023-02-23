30ea984be7d10e079a5388e1d764e8a954416c2d
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-23 03:50:43 -0300

    basico da function

---
+++ anotações/basico da function.js
@@ -0,0 +1,15 @@
+//criar um aplicativo de frases motivacionais
+
+//declaration - declaração da função
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+
+//executar a função
+createPhrases()
+createPhrases()
+
+console.log('fim do programa')
