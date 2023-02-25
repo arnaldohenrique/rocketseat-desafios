c75c63484692a53367bae6eeed868ad01420cd84
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-25 18:48:42 -0300

    typecasting

---
+++ anotações/script.js
@@ -0,0 +1,11 @@
+/*
+    Type conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+*/
+
+//type coersion - JS transformou o 5(number) em 5(string) para fazer a concatenação sem problemas. 
+console.log('9' + 5)
+
+//type conversion - 
+console.log((Number)'9' + 5)
