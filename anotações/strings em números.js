6551bc0b93a773a5a15139f258e1a61aa725eeb4
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-03-25 04:17:34 -0300

    strings em números

---
+++ anotações/strings em números.js
@@ -0,0 +1,9 @@
+//Manipulando Strings e Números
+
+// Transformar String em Número e Número em String
+
+let sting = '123'
+console.log(Number(string)) //transformando em number
+
+let number = 321
+console.log(String(number)) //transformando em string
