728d15ade6c9952db94a7da4ae61cc722f20593d
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-03-25 04:33:11 -0300

    Maiúsculas e minúsculas

---
+++ anotações/maiúsculas e minúsculas.js
@@ -0,0 +1,9 @@
+//Manipulando Strings e Números
+
+//Transforme letras minúsculas em maiúsculas. Faça o contrário disso tambem
+
+let word = 'Programar É Muito Bacana'
+
+console.log(word.toLowerCase()) //letras minúculas
+
+console.log(word.toUpperCase()) //letras maiúsculas
