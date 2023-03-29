36a2a3e5062d37ccf80fd4d979354bad9f2be15d
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-03-29 02:02:07 -0300

    elementos do array

---
+++ anotações/elementos do array.js
@@ -0,0 +1,13 @@
+//Manipulando Arrays
+
+//Contar elementos de um array
+
+console.log(['a', 'b', 'c'].length)
+
+//com outros objetos
+
+console.log([
+    'a',
+    {type: 'array'},
+    function() { return 'alo'},
+])
