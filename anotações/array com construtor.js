275533d7ed453adf4bdd83dd1b6a9a46ba22354e
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-03-29 01:56:23 -0300

    array com construtor

---
+++ anotações/array com construtor.js
@@ -0,0 +1,10 @@
+//Manipulando Arrays
+
+//Criar Array com construtor
+
+let myArray = new Array('a', 'b', 'c')
+console.log(myArray)
+
+//você pode criar um Array com contrutor vazio
+let myArray2 = new Array(10)
+console.log(myArray2)
