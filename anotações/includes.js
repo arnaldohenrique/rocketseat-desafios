363cf2f4f2b0ad0ce5997d2ca4373410c07bf9fe
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-03-29 01:52:14 -0300

    includes

---
+++ anotações/includes.js
@@ -0,0 +1,8 @@
+//Manipulando Strings
+
+//Verificar se o texto contém a palavra Amor
+
+let phrase = "Eu quero viver o amor!"
+console.log(phrase.includes("Amor")) 
+//lembrando que letras maiúsculas e minúsculas importa
+console.log(phrase.includes("amor"))
