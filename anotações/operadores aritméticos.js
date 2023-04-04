52512247c5006f485cdd99a67116da3ba9bf6fbe
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-04-04 06:10:28 -0300

    Operadores aritméticos

---
+++ anotações/operadores aritméticos.js
@@ -0,0 +1,37 @@
+/* 
+    Falaremos nessa aula sobre operadores aritméticos, que nos possibilitam fazer cálculos matemáticos. 
+*/
+
+//O operador de multiplicação é o * (asterisco)
+console.log(10*10)
+
+//O operador de divisão é a / (barra)
+console.log(10/10)
+
+//O operador de soma é o + (positivo)
+console.log(10+10)
+
+//O operador de subtração é o - (negativo)
+console.log(10-10)
+
+/* 
+    Outros operadores aritméticos incluem o resto de divisão, incremento, decremento e exponencial, com exemplos a seguir: 
+*/
+
+// resto da divisão: sinal %
+let remainder
+remainder = 11 % 9
+console.log(remainder)
+
+// incremento: sinal ++
+let increment = 0
+increment++
+console.log(increment)
+
+// decremento: sinal --
+let decrement = 0
+decrement--
+console.log(decrement)
+
+// exponencial: sinal **
+console.log(2 ** 3)
