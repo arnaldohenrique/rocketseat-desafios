a59df7855192800ca6ad4a4a52d978a827b5d6e4
Arnaldo H. Saito <contato.arnaldohenrique@gmail.com>
2023-07-18 17:13:22 -0300

    Operadores para string

---
+++ anotações/operadores para string.js
@@ -0,0 +1,9 @@
+// Operador de String (String operator)
+
+// comparison (comparação)
+console.log( 'a' == 'a')
+
+//concatenation (concatenação)
+//retorna a união de duas Strings
+let alpha = 'alpha'
+console.log(alpha + 'bet')
