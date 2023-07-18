8b3fdf927fc0122d6559d08420b07c0f5e3d0ac1
Arnaldo H. Saito <contato.arnaldohenrique@gmail.com>
2023-07-18 16:29:18 -0300

    Operadores de comparação maior e menor (igual)

---
+++ anotações/operadores de comparação maior e menor (igual).js
@@ -0,0 +1,20 @@
+let one = 1
+let two = 2
+
+//      >    MAIOR QUE
+// console.log(one>two) - false
+
+
+//      >=  MAIOR OU IGUAL A
+//console.log(one >= 1) - true
+//console.log(two >= 1) - true
+
+
+//      <   MENOR QUE
+//console.log(one < two) - true
+
+
+//      <=  MENOR OU IGUAL A
+//console.log(one <= two) - true
+//console.log(one <= 1) - true
+//console.log(one <= 0) - false
