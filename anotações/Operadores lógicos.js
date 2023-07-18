5c11fc1fcdb98b1b64c10d013268819403df335e
Arnaldo H. Saito <contato.arnaldohenrique@gmail.com>
2023-07-18 16:42:16 -0300

    operadores lógicos

---
+++ anotações/Operadores lógicos.js
@@ -0,0 +1,16 @@
+//  Operadores lógicos (logical operators)
+
+
+//  - 2 valores booleanos, quando  verificados, resultara em VERDADEIRO ou FALSO
+
+let pao = true
+let queijo = true
+
+// AND &&
+// console.log(pao && queijo)
+
+// OR ||
+// console.log(pao || queijo)
+
+// NOT !
+// console.log(!pao)
